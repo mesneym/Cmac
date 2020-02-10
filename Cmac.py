f10af90d4e20c568e91edddd56415b04046e48c7
mesneym <mesney.mesney@gmail.com>
2020-02-10 14:34:24 -0500

    Need To Tweak parameters

--- Cmac.py
+++ Cmac.py
@@ -12,9 +12,11 @@ class Cmac:
 
 
     def __kernel(self,x1,x2):
-        return 1/abs(x1-x2)
-        # return 1/(x1-x2)**2
-        # return 1/np.exp((x1-x2)**2/4)
+        # if(x1 == x2):
+            # return 1
+        # return 1/((x1-x2)**2)
+        # return 1/abs(x1-x2)
+        return 1/np.exp((x1-x2)**2/4)
 
 
 
@@ -24,7 +26,6 @@ class Cmac:
 
         quantization = int(np.floor(self.resolution*(x-xmin)/(xmax-xmin)) + 
                        np.floor(self.generalization/2))
-        quant = int(np.floor(self.resolution*(x-xmin)/(xmax-xmin)))
         low = int(quantization -np.floor(self.generalization/2))
         high= int(quantization +np.floor(self.generalization/2))
         neighborOfx = (low,high)
@@ -44,21 +45,22 @@ class Cmac:
     def __updateWeights(self,x,learningRate,error,xmin,xmax):
         quantization = int(np.floor(self.resolution*(x-xmin)/(xmax-xmin)) + 
                        np.floor(self.generalization/2))
-        quant = int(np.floor(self.resolution*(x-xmin)/(xmax-xmin)))
         low = int(quantization -np.floor(self.generalization/2))
         high= int(quantization +np.floor(self.generalization/2))
         neighborOfx = (low,high)
 
         
         for i in range(neighborOfx[0],neighborOfx[1]+1):
-            self.weights[i] -= learningRate*error*self.__kernel(quantization,self.weights[i])
-
-
+            self.weights[i] += learningRate*error*self.__kernel(quantization,i)
 
     def train(self,x,y,learningRate=0.02,iterations=2000,accuracy=0.01,d = 0, xmin=0 ,xmax =10):
         for i in range(len(x)):
             for j in range(iterations):
-                error = y[i] - self.prediction(x[i],xmin,xmax) 
+                error = y[i]-self.prediction(x[i],xmin,xmax) 
+                print("prediction is {}".format(self.prediction(x[i],xmin,xmax)))
+                print("error is{}".format(error))
+                print("==========")
+                print(" ")
                 if(error < accuracy):
                     break
                 self.__updateWeights(x[i],learningRate,error, xmin,xmax)
@@ -67,10 +69,10 @@ class Cmac:
                 
 
         
-# a = Cmac(5,35,35)
-# x = np.array([0, 1, 2])
-# x.reshape(len(x),1)
-# y = np.cos(x) 
+a = Cmac(5,35,35)
+x = np.array([0, 1, 2])
+x.reshape(len(x),1)
+y = np.cos(x) 
 
-# a.train(x,y)
-# print(a.prediction(x[2]))
+a.train(x,y)
+print(a.prediction(x[2]))
