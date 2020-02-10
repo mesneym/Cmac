75a4846118d11b6517a4d4e2f9d2836f93b88cd4
mesneym <mesney.mesney@gmail.com>
2020-02-09 21:13:50 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -9,22 +9,24 @@ trainData = np.column_stack((x,y))
 x = np.random.uniform(0,10,(30,1))
 y = np.sin(x)
 
+testDataExpected = np.column_stack((x,y))
+
 
 # Train and test cmac code goes here
-
-testData = np.column_stack((x,y))
-
+testDataResults = np.column_stack((x,y))
 #
+
 
 
 fig1, ax1 = plt.subplots()
 ax1.plot(trainData[:,0],trainData[:,1],'ro',label = 'Training Data')
+ax1.plot(testDataResults[:,0],testDataResults[:,1],'go',label = 'Expected Output from Test Data')
+ax1.plot(testDataExpected[:,0],testDataExpected[:,1],'b^',label = 'Cmac Output from Test Data')
+
 ax1.set_title("CMAC Results")
 ax1.set_xlabel("X-label for axis 1")
 ax1.set_ylabel("sin(x)")
 
 
-
-ax1.plot(testData[:,0],testData[:,1],'go',label = 'Test Data')
 plt.legend(loc = "upper right")
 plt.show()
